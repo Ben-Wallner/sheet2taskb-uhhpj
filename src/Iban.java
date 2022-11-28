3f7574fd6059db87c6e74ef2d5bc477a03f7b502
Ben Biber <24373331+Ben-Wallner@users.noreply.github.com>
2022-11-28 14:41:47 +0100

    Update

--- src/Iban.java
+++ src/Iban.java
@@ -3,95 +3,58 @@ import java.math.BigInteger;
 public class Iban {
     String iban;
     String accountNumber;
-    String countryRecognition;
+    String countryCode;
     String bankCode;
-    
     public Iban(String country, String bankCode2, String account) {
-        this.accountNumber = account;
-        this.countryRecognition = country;
-        this.bankCode = bankCode2;
-        this.iban = splitBan();
+        accountNumber = account;
+        countryCode = country;
+        bankCode = bankCode2;
+        iban = splitBan();
         }
-
-    public String accountNumber10() {
-    for (int i = accountNumber.length(); i < 10; i++) {
-    accountNumber = "0" + accountNumber;
+    public String generateBban() {
+        for (int i = accountNumber.length(); i < 10; i++) {
+            accountNumber = "0" + accountNumber;
     }
-    return accountNumber;
+        return bankCode + accountNumber;
     }
-    
-    //Zusammenfügen BLZ und Kontonr
-    public String bankCodeAccount() {
-    
-    return bankCode + accountNumber10();
-    }
-    
-    //Länderkenz in Zahlen
     public int letterToNumber(char a) {
-    int number = a - 64;
-    number += 9;
-    
-    return number;
-    
+        int number = a - 64;
+        number += 9;
+        return number;
     }
-    
-    //Zusammenfügen der beiden BuchstabenZahlen
     public String country() {
-    String partOne = Integer.toString(letterToNumber(this.countryRecognition.charAt(0)));
-    String partTwo = Integer.toString(letterToNumber(this.countryRecognition.charAt(1)));
-    
-    return partOne + partTwo + "00";
-    
+        String first = Integer.toString(letterToNumber(countryCode.charAt(0)));
+        String second = Integer.toString(letterToNumber(countryCode.charAt(1)));
+        return first + second + "00";
     }
     
-    
-    //Zusammenfügen von allem
     public String bankCodeAccountCountry() {
-    
-    return bankCodeAccount() + country();
+        return generateBban() + country();
     }
-    
-    
-    //Prüfsumme
-    public String finalCheckSum() {
-    
-    //Zahlen in BigInt und Modulo und Substrahieren
-    BigInteger firstIban = new BigInteger(bankCodeAccountCountry());
-    BigInteger modNumber = new BigInteger("97");
-    BigInteger subtractor = new BigInteger("98");
-    
-    BigInteger test = firstIban.mod(modNumber);
-    BigInteger test1 = subtractor.subtract(test);
-    
-    
-    //BigInt zurück in Int sonst geht nix
-    
-    int checkSum = test1.intValue();
-    
-    //falls Prüfsumme nicht 2Stellig ist, eine 0 hinzufügen
-    if (checkSum < 10) {
-    return countryRecognition + "0" + checkSum + bankCodeAccount();
+    public String checkSum() {
+        BigInteger firstIban = new BigInteger(bankCodeAccountCountry());
+        BigInteger modNumber = new BigInteger("97");
+        BigInteger subtractor = new BigInteger("98");
+        BigInteger test = firstIban.mod(modNumber);
+        BigInteger test1 = subtractor.subtract(test);    
+        int checkSum = test1.intValue();
+        if (checkSum < 10) {
+            return countryCode + "0" + checkSum + generateBban();
+        }
+        else {
+            return countryCode + checkSum + generateBban();
+        } 
+        } 
+        public String splitBan() {
+        String s1 = checkSum().substring(0, 4);
+        String s2 = checkSum().substring(4, 8);
+        String s3 = checkSum().substring(8, 12);
+        String s4 = checkSum().substring(12, 16);
+        String s5 = checkSum().substring(16, 20);
+        String s6 = checkSum().substring(20, 22);
+        return s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6;
     }
-    else {
-    return countryRecognition + checkSum + bankCodeAccount();
-    } 
-    } 
-    public String splitBan() {
-    
-    String s1 = finalCheckSum().substring(0, 4);
-    String s2 = finalCheckSum().substring(4, 8);
-    String s3 = finalCheckSum().substring(8, 12);
-    String s4 = finalCheckSum().substring(12, 16);
-    String s5 = finalCheckSum().substring(16, 20);
-    String s6 = finalCheckSum().substring(20, 22);
-    
-    return s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6;
-    }
-    
-    /**
-    * Stellt die fertige IBAN zu verfügung
-    */
-    public void printIban() {
-    System.out.println(this.iban); 
+    public void generateIban() {
+        System.out.println(iban); 
     } 
     }
